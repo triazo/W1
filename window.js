bfacef8137ac36667280db99ba46e72885c7979f
Adam Freeman <adam@triazo.net>
2016-01-06 00:55:50 -0500

    Add key to toggle help menu

--- window.js
+++ window.js
@@ -1,11 +1,23 @@
 'use strict';
 
+function toggleHelp() {
+    var hDiv = document.getElementById("help");
+    if (hDiv.style.display == "block") {
+	hDiv.style.display = "none";
+    }
+    else {
+	hDiv.style.display = "block";
+    }
+}
+
 function keyHandler(event) {
     console.log(event.keyCode);
 
-
     // Keycodes:
     // 112: F1
+    if (event.keyCode == 112) {
+	toggleHelp()
+    }
 
     // 122: F11
     // 70:  F
